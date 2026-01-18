6c4fc9d67e05e4af11c88f1ff60886c29d124bba
Johnny Hammond <30871397+Pythonidaer@users.noreply.github.com>
2026-01-18 13:59:37 -0500

    installed vite visualizer to identify bulky loads in the main bundle. code split LatestBlogs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react";
 import { TopBanner } from "../sections/TopBanner";
 import { Header } from "../sections/Header";
 import { Hero } from "../sections/Hero";
@@ -6,10 +7,13 @@ import { PlatformIntro } from "../sections/PlatformIntro";
 import { AgencyLogos } from "../sections/AgencyLogos";
 import { FeatureAccordion } from "../sections/FeatureAccordion";
 import { CustomerSpotlight } from "../sections/CustomerSpotlight";
-import { LatestBlogs } from "../sections/LatestBlogs";
 import { CampaignBanner } from "../sections/CampaignBanner";
 import { Footer } from "../sections/Footer";
 import { MetaTags } from "../components/MetaTags";
+
+// Lazy load LatestBlogs to move blog JSON files out of main bundle
+// Note: LatestBlogs is a named export, so we need to map it to default
+const LatestBlogs = lazy(() => import("../sections/LatestBlogs").then(module => ({ default: module.LatestBlogs })));
 
 export function Home() {
   return (
@@ -28,7 +32,9 @@ export function Home() {
       <AgencyLogos />
       <FeatureAccordion />
       <CustomerSpotlight />
-      <LatestBlogs />
+      <Suspense fallback={<div>Loading blog preview...</div>}>
+        <LatestBlogs />
+      </Suspense>
       <CampaignBanner />
       <Footer />
     </main>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { asyncCss } from "./vite-plugin-async-css";
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     asyncCss(), // Make CSS load asynchronously (non-blocking)
+    visualizer({
+      open: true,
+      filename: 'dist/stats.html',
+      gzipSize: true,
+      brotliSize: true,
+    }),
   ],
 });
