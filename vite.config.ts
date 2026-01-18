7ebfc381208ec0d821918a889e4314efaf707df1
Johnny Hammond <30871397+Pythonidaer@users.noreply.github.com>
2026-01-18 10:34:38 -0500

    testing to see if asynchronous CSS loading allows HTML parsing and rendering to continue, unblocking without making the screen look weird

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { asyncCss } from "./vite-plugin-async-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    asyncCss(), // Make CSS load asynchronously (non-blocking)
+  ],
 });
